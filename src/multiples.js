ab027e266c76a86b578cd4e5ef4ea79b13663801
yaroslavs skiba <yaroslavskiba322@gmail.com>
2022-10-03 21:29:31 +0300

    multiples

---
+++ src/multiples.js
@@ -0,0 +1,8 @@
+function solution(number){
+  let result = 0;
+  for (let i = number - 1; i > 0; i--) {
+    if (i % 3 === 0 || i % 5 === 0 || i % 3 === 0 && i % 5 === 0)
+      result += i;
+  }
+  return result;
+}
